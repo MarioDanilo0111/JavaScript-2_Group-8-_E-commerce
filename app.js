4bf2294f2d6bc490a7c66fc57fc3414ff5bf20ba
964Code <964code@gmail.com>
2022-01-07 10:41:21 +0100

    Wrote JS-code to populate products with HTML-template

--- app.js
+++ app.js
@@ -1,96 +1,106 @@
-// Save .JSON-file locally
-/* let object;
-let httpRequest = new XMLHttpRequest();
-httpRequest.open("GET", "./productDB.json", true);
-httpRequest.send();
-httpRequest.addEventListener("readystatechange", function () {
-  if (this.readyState === this.DONE) {
-    object = JSON.parse(this.response);
-  }
-}); */
+const proDatabase = [
+  {
+    id: 1,
+    name: "Nike Tshirt",
+    price: 200,
+    category: "Activewear",
+    gender: "unisex",
+    brand: "Nike",
+    img: "./img/shoe.jpg",
+  },
+  {
+    id: 2,
+    name: "Adidas Sweatpants",
+    price: 149,
+    category: "Sportswear",
+    gender: "Male",
+    brand: "Adidas",
+    img: "./img/shoe.jpg",
+  },
+  {
+    id: 3,
+    name: "Nike Tshirt",
+    price: 200,
+    category: "Activewear",
+    gender: "unisex",
+    brand: "Nike",
+    img: "./img/shoe.jpg",
+  },
+  {
+    id: 4,
+    name: "Adidas Sweatpants",
+    price: 149,
+    category: "Sportswear",
+    gender: "Male",
+    brand: "Adidas",
+    img: "./img/shoe.jpg",
+  },
+  {
+    id: 5,
+    name: "Nike Tshirt",
+    price: 200,
+    category: "Activewear",
+    gender: "unisex",
+    brand: "Nike",
+    img: "./img/shoe.jpg",
+  },
+  {
+    id: 6,
+    name: "Adidas Sweatpants",
+    price: 149,
+    category: "Sportswear",
+    gender: "Male",
+    brand: "Adidas",
+    img: "./img/shoe.jpg",
+  },
+  {
+    id: 7,
+    name: "Nike Tshirt",
+    price: 200,
+    category: "Activewear",
+    gender: "unisex",
+    brand: "Nike",
+    img: "./img/shoe.jpg",
+  },
+  {
+    id: 8,
+    name: "Adidas Sweatpants",
+    price: 149,
+    category: "Sportswear",
+    gender: "Male",
+    brand: "Adidas",
+    img: "./img/shoe.jpg",
+  },
+  {
+    id: 9,
+    name: "Adidas Sweatpants",
+    price: 139,
+    category: "Sportswear",
+    gender: "Male",
+    brand: "Rebook",
+    img: "./img/shoe.jpg",
+  },
+];
+const cartNum = document.querySelector(".cart-length");
+const productContainer = document.querySelector(".grid-container");
 
-/* let jsondata;
-fetch("./productDB.json")
-  .then(function (u) {
-    return u.json();
-  })
-  .then(function (json) {
-    jsondata = json;
+// Create Product HTML-templae when on product page
+if (window.location.pathname.includes("/product-page.html")) {
+  console.log("You are on Product Page, loading products.");
+  proDatabase.forEach((obj) => {
+    /*    console.log(`${obj}`); */
+    productContainer.innerHTML += `
+      <div class="grid-item grid-item-${obj.id}">
+      <img src="${obj.img}" alt="${obj.name} ${obj.gender}" srcset="">
+      <p class=${obj.id}>${obj.name}</p>
+      <a href="#">Details</a>
+      </div>`;
   });
- */
+} else {
+  console.log("Your are not on Product Page");
+}
 
-/* fetch only works if .json is looks like this, it's complaining about ":" that comes after products [
-  {
-    "id": 1,
-    "name": "Nike Tshirt",
-    "price": 200,
-    "category": "Activewear",
-    "gender": "unisex",
-    "brand": "Nike"
-  },
-  {
-    "id": 2,
-    "name": "Adidas Sweatpants",
-    "price": 149,
-    "category": "Sportswear",
-    "gender": "Male",
-    "brand": "Adidas"
-  },
-  {
-    "id": 3,
-    "name": "Nike Tshirt",
-    "price": 200,
-    "category": "Activewear",
-    "gender": "unisex",
-    "brand": "Nike"
-  },
-  {
-    "id": 4,
-    "name": "Adidas Sweatpants",
-    "price": 149,
-    "category": "Sportswear",
-    "gender": "Male",
-    "brand": "Adidas"
-  },
-  {
-    "id": 5,
-    "name": "Nike Tshirt",
-    "price": 200,
-    "category": "Activewear",
-    "gender": "unisex",
-    "brand": "Nike"
-  },
-  {
-    "id": 6,
-    "name": "Adidas Sweatpants",
-    "price": 149,
-    "category": "Sportswear",
-    "gender": "Male",
-    "brand": "Adidas"
-  },
-  {
-    "id": 7,
-    "name": "Nike Tshirt",
-    "price": 200,
-    "category": "Activewear",
-    "gender": "unisex",
-    "brand": "Nike"
-  },
-  {
-    "id": 8,
-    "name": "Adidas Sweatpants",
-    "price": 149,
-    "category": "Sportswear",
-    "gender": "Male",
-    "brand": "Adidas"
-  },
-  {
-    "id": 9,
-    "name": "Adidas Sweatpants",
-    "price": 139,
-    "category": "Sportswear",
-    "gender": "Male",
-    "brand": "Rebook"
-  }
-]
-*/
+// Shopping Cart Display Number Func
+/* addEventListener("click", () => {
+  cartNum.innerHTML = `${proDatabase.length}`;
+}); */
